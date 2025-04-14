870c1d8a912ca6e80d8dd65b87a8c77836827774
DubathF <florian.dubath@unige.ch>
2025-04-14 17:09:25 +0200

    MNT: Re-rendered with conda-build 25.3.1, conda-smithy 3.47.1, and conda-forge-pinning 2025.04.13.21.56.35

--- .scripts/run_docker_build.sh
+++ .scripts/run_docker_build.sh
@@ -12,7 +12,7 @@ source .scripts/logging_utils.sh
 set -xeo pipefail
 
 THISDIR="$( cd "$( dirname "$0" )" >/dev/null && pwd )"
-PROVIDER_DIR="$(basename $THISDIR)"
+PROVIDER_DIR="$(basename "$THISDIR")"
 
 FEEDSTOCK_ROOT="$( cd "$( dirname "$0" )/.." >/dev/null && pwd )"
 RECIPE_ROOT="${FEEDSTOCK_ROOT}/recipe"
@@ -110,4 +110,3 @@ test -f "$DONE_CANARY"
 
 # This closes the last group opened in `build_steps.sh`
 ( endgroup "Final checks" ) 2> /dev/null
-
